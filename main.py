f39ac2a47c46c492e945157f4d41c0c493ea1783
Kapi <michaelpo88@mail.ru>
2025-01-17 21:49:57 +0300

    First Commit

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pygame
+import os
+import sys
 
 
-# Press the green button in the gutter to run the script.
+def load_image(name, color_key=None):
+    fullname = os.path.join('data/image', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print("Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+class Cursor(pygame.sprite.Sprite):
+    cat_cursor = load_image("Cat_cursor.png")
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = Cursor.cat_cursor
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+
+    def update(self, *args):
+        cord = args[0].pos
+        self.rect.x = cord[0]
+        self.rect.y = cord[1]
+
+
+def main():
+    def draw():
+        cursor.draw(screen)
+
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEMOTION and pygame.mouse.get_focused():
+                cursor.update(event)
+
+        draw()
+        pygame.display.flip()
+    pygame.quit()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pygame.init()
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+    cursor = pygame.sprite.Group()
+    pygame.mouse.set_visible(False)
+    Cursor(cursor)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
