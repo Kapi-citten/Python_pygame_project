0476b9a1b34c58e2d5fe75fc679f9b05717e65c2
Kapi <michaelpo88@mail.ru>
2025-01-19 02:44:30 +0300

    Diagonal walk

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 
 def load_image(name, color_key=None):
-    fullname = os.path.join('data/image', name)
+    fullname = os.path.join("data/image", name)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
@@ -29,31 +29,35 @@ class Cursor(pygame.sprite.Sprite):
         self.rect.y = cord[1]
 
 
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, texture_path, group):
+        super().__init__(group)
+        self.image = load_image(texture_path)
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def check_collision(self, sprite):
+        return self.rect.colliderect(sprite.rect)
+
+
 class Hero(pygame.sprite.Sprite):
     image = load_image("hero/down/move_s_1.png")
 
     def __init__(self, group):
         super().__init__(group)
 
-        self.hero_d = [load_image('hero/right/move_d_1.png'), load_image('hero/right/move_d_2.png'),
-                  load_image('hero/right/move_d_3.png'), load_image('hero/right/move_d_4.png'),
-                  load_image('hero/right/move_d_5.png'), load_image('hero/right/move_d_6.png'),
-                  load_image('hero/right/move_d_7.png'), load_image('hero/right/move_d_8.png')]
+        self.hero_d = [load_image(f"hero/right/move_d_{i}.png") for i in range(1, 9)]
+        self.hero_a = [load_image(f"hero/left/move_a_{i}.png") for i in range(1, 9)]
+        self.hero_s = [load_image(f"hero/down/move_s_{i}.png") for i in range(1, 9)]
+        self.hero_w = [load_image(f"hero/up/move_w_{i}.png") for i in range(1, 9)]
 
-        self.hero_a = [load_image('hero/left/move_a_1.png'), load_image('hero/left/move_a_2.png'),
-                  load_image('hero/left/move_a_3.png'), load_image('hero/left/move_a_4.png'),
-                  load_image('hero/left/move_a_5.png'), load_image('hero/left/move_a_6.png'),
-                  load_image('hero/left/move_a_7.png'), load_image('hero/left/move_a_8.png')]
+        self.hero_wd = [load_image(f"hero/up_right/move_w_d_{i}.png") for i in range(1, 9)]
+        self.hero_wa = [load_image(f"hero/up_left/move_w_a_{i}.png") for i in range(1, 9)]
+        self.hero_sa = [load_image(f"hero/down_left/move_s_a_{i}.png") for i in range(1, 9)]
+        self.hero_sd = [load_image(f"hero/down_right/move_s_d_{i}.png") for i in range(1, 9)]
 
-        self.hero_s = [load_image('hero/down/move_s_1.png'), load_image('hero/down/move_s_2.png'),
-                  load_image('hero/down/move_s_3.png'), load_image('hero/down/move_s_4.png'),
-                  load_image('hero/down/move_s_5.png'), load_image('hero/down/move_s_6.png'),
-                  load_image('hero/down/move_s_7.png'), load_image('hero/down/move_s_8.png')]
-
-        self.hero_w = [load_image('hero/up/move_w_1.png'), load_image('hero/up/move_w_2.png'),
-                  load_image('hero/up/move_w_3.png'), load_image('hero/up/move_w_4.png'),
-                  load_image('hero/up/move_w_5.png'), load_image('hero/up/move_w_6.png'),
-                  load_image('hero/up/move_w_7.png'), load_image('hero/up/move_w_8.png')]
 
         self.image = Hero.image
         self.rect = self.image.get_rect()
@@ -61,39 +65,78 @@ class Hero(pygame.sprite.Sprite):
         self.rect.y = 0
         self.mov_index = 0
 
-    def update(self, mov):
+    def update(self, mov, walls_group):
         # mov = args[0].key
+        old_rect = self.rect.copy()
 
-        if mov == 119:
+        if mov == 'wd':
+            self.image = self.hero_wd[self.mov_index // 8]
+            self.rect = self.rect.move(speed, -speed)
+
+        elif mov == 'wa':
+            self.image = self.hero_wa[self.mov_index // 8]
+            self.rect = self.rect.move(-speed, -speed)
+
+        elif mov == 'sa':
+            self.image = self.hero_sa[self.mov_index // 8]
+            self.rect = self.rect.move(-speed, speed)
+
+        elif mov == 'sd':
+            self.image = self.hero_sd[self.mov_index // 8]
+            self.rect = self.rect.move(speed, speed)
+
+        elif mov == 'w':
             self.image = self.hero_w[self.mov_index // 8]
-            self.rect = self.rect.move(0, -3)
+            self.rect = self.rect.move(0, -speed)
 
-        elif mov == 115:
+        elif mov == 's':
             self.image = self.hero_s[self.mov_index // 8]
-            self.rect = self.rect.move(0, 3)
+            self.rect = self.rect.move(0, speed)
 
-        elif mov == 97:
+        elif mov == 'a':
             self.image = self.hero_a[self.mov_index // 8]
-            self.rect = self.rect.move(-3, 0)
+            self.rect = self.rect.move(-speed, 0)
 
-        elif mov == 100:
+        elif mov == 'd':
             self.image = self.hero_d[self.mov_index // 8]
-            self.rect = self.rect.move(3, 0)
+            self.rect = self.rect.move(speed, 0)
+
+
+
 
         else:
             self.image = self.hero_s[0]
+
         self.mov_index += 1
         if self.mov_index >= 64:
             self.mov_index = 0
+
+        for wall in walls_group:
+            if wall.check_collision(self):
+                self.rect = old_rect
+
 
 def m():
     main_hero = pygame.sprite.Group()
     Hero(main_hero)
     running = True
+    walls_group = pygame.sprite.Group()
+    texture_path = "hero/down/move_s_1.png"
+    walls_data = [
+        (100, 100, 50, 200),
+        (300, 50, 100, 50),
+        (500, 300, 150, 50),
+        (200, 400, 50, 150),
+    ]
+
+    for wall_data in walls_data:
+        x, y, width, height = wall_data
+        Wall(x, y, width, height, texture_path, walls_group)
 
     def draw():
-        cursor.draw(screen)
         main_hero.draw(screen)
+        walls_group.draw(screen)
+        cursor.draw(screen)
 
     while running:
         screen.fill((0, 0, 0))
@@ -105,20 +148,33 @@ def m():
                 cursor.update(event)
 
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP] or pressed[pygame.K_w]:
-            main_hero.update(119)
+
+        if (pressed[pygame.K_UP] or pressed[pygame.K_w]) and (pressed[pygame.K_RIGHT] or pressed[pygame.K_d]):
+            main_hero.update('wd', walls_group)
+
+        elif (pressed[pygame.K_UP] or pressed[pygame.K_w]) and (pressed[pygame.K_LEFT] or pressed[pygame.K_a]):
+            main_hero.update('wa', walls_group)
+
+        elif (pressed[pygame.K_DOWN] or pressed[pygame.K_s]) and (pressed[pygame.K_RIGHT] or pressed[pygame.K_d]):
+            main_hero.update('sd', walls_group)
+
+        elif (pressed[pygame.K_DOWN] or pressed[pygame.K_s]) and (pressed[pygame.K_LEFT] or pressed[pygame.K_a]):
+            main_hero.update('sa', walls_group)
+
+        elif pressed[pygame.K_UP] or pressed[pygame.K_w]:
+            main_hero.update('w', walls_group)
 
         elif pressed[pygame.K_RIGHT] or pressed[pygame.K_d]:
-            main_hero.update(100)
+            main_hero.update('d', walls_group)
 
         elif pressed[pygame.K_DOWN] or pressed[pygame.K_s]:
-            main_hero.update(115)
+            main_hero.update('s', walls_group)
 
         elif pressed[pygame.K_LEFT] or pressed[pygame.K_a]:
-            main_hero.update(97)
+            main_hero.update('a', walls_group)
 
         else:
-            main_hero.update(None)
+            main_hero.update(None, walls_group)
 
         clock.tick(fps)
         draw()
@@ -126,10 +182,12 @@ def m():
     pygame.quit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pygame.init()
     size = width, height = 800, 400
     screen = pygame.display.set_mode(size)
+
+    speed = 2
 
     fps = 60
     clock = pygame.time.Clock()
