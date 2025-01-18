d7415722d64157013a972a668a35f8a853a95b06
Kapi <michaelpo88@mail.ru>
2025-01-18 21:49:24 +0300

    Commented Hero class

--- main.py
+++ main.py
@@ -29,32 +29,50 @@ class Cursor(pygame.sprite.Sprite):
         self.rect.y = cord[1]
 
 
-# class Hero(pygame.sprite.Sprite):
-#     image = load_image("hero_stay.png")
-#
-#     def __init__(self, group):
-#         super().__init__(group)
-#         hero_d = [load_image('hero_d_1.png'), load_image('hero_d_2.png'), load_image('hero_d_3.png'), load_image('hero_d_4.png')]
-#         hero_a = [load_image('hero_a_1.png'), load_image('hero_a_2.png'), load_image('hero_a_3.png'), load_image('hero_a_4.png')]
-#         hero_s = [load_image('hero_s_1.png'), load_image('hero_s_2.png'), load_image('hero_s_3.png'), load_image('hero_s_4.png')]
-#         hero_w = [load_image('hero_w_1.png'), load_image('hero_w_2.png'), load_image('hero_w_3.png'), load_image('hero_w_4.png')]
-#         self.image = Hero.image
-#         self.rect = self.image.get_rect()
-#         self.rect.x = 0
-#         self.rect.y = 0
-#
-#     def update(self, mov):
-#         # mov = args[0].key
-#         if mov == 119:
-#             self.rect = self.rect.move(0, -10)
-#         elif mov == 115:
-#             self.rect = self.rect.move(0, 10)
-#         elif mov == 97:
-#             self.image = load_image("K.png")
-#             self.rect = self.rect.move(-10, 0)
-#         elif mov == 100:
-#             self.image = load_image("K_r.png")
-#             self.rect = self.rect.move(10, 0)
+class Hero(pygame.sprite.Sprite):
+    image = load_image("hero_stay.png")
+
+    def __init__(self, group):
+        super().__init__(group)
+
+        hero_d = [load_image('hero/down/move_d_1.png'), load_image('hero/down/move_d_2.png'),
+                  load_image('hero/down/move_d_3.png'), load_image('hero/down/move_d_4.png'),
+                  load_image('hero/down/move_d_5.png'), load_image('hero/down/move_d_6.png'),
+                  load_image('hero/down/move_d_7.png'), load_image('hero/down/move_d_8.png')]
+
+        hero_a = [load_image('hero/down/move_a_1.png'), load_image('hero/down/move_a_2.png'),
+                  load_image('hero/down/move_a_3.png'), load_image('hero/down/move_a_4.png'),
+                  load_image('hero/down/move_a_5.png'), load_image('hero/down/move_a_6.png'),
+                  load_image('hero/down/move_a_7.png'), load_image('hero/down/move_a_8.png')]
+
+        hero_s = [load_image('hero/down/move_s_1.png'), load_image('hero/down/move_s_2.png'),
+                  load_image('hero/down/move_s_3.png'), load_image('hero/down/move_s_4.png'),
+                  load_image('hero/down/move_s_5.png'), load_image('hero/down/move_s_6.png'),
+                  load_image('hero/down/move_s_7.png'), load_image('hero/down/move_s_8.png')]
+
+        hero_w = [load_image('hero/down/move_w_1.png'), load_image('hero/down/move_w_2.png'),
+                  load_image('hero/down/move_w_3.png'), load_image('hero/down/move_w_4.png'),
+                  load_image('hero/down/move_w_5.png'), load_image('hero/down/move_w_6.png'),
+                  load_image('hero/down/move_w_7.png'), load_image('hero/down/move_w_8.png')]
+
+        self.image = Hero.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+        self.mov_index = 0
+
+    def update(self, mov):
+        # mov = args[0].key
+        if mov == 119:
+            self.rect = self.rect.move(0, -10)
+        elif mov == 115:
+            self.rect = self.rect.move(0, 10)
+        elif mov == 97:
+            self.image = load_image("K.png")
+            self.rect = self.rect.move(-10, 0)
+        elif mov == 100:
+            self.image = load_image("K_r.png")
+            self.rect = self.rect.move(10, 0)
 
 
 def m():
